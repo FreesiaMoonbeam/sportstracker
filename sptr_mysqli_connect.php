6ef391a0727bef3116c8eb8593099f0bcb4250de
Gian Mauricio <gian_mauricio@dlsu.edu.ph>
2021-02-18 17:53:20 +0800

    Linking to the DB works

--- sptr_mysqli_connect.php
+++ sptr_mysqli_connect.php
@@ -1,13 +1,30 @@
 <?php
 
-DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PASSWORD', '');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'sportstracker_db');
+#DEFINE ('DB_USER', 'root');
+#DEFINE ('DB_PASSWORD', '');
+#DEFINE ('DB_HOST', 'localhost');
+#DEFINE ('DB_NAME', 'sportstracker_db');
+
+$DBServer = "localhost:3306";
+$DBUser = "root";
+$DBPwd = "!ntenseAvar1ce";
+$DBName = "scoretracker";
+
+$connection = mysqli_connect($DBServer, $DBUser, $DBPwd, $DBName);
 
 
-$dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-OR die ('Could not connect to
- MySQL: ' . mysqli_connect_error( ) );
+#$dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
+#if(!$dbc){
+#    echo "'Could not connect to
+#     MySQL on Shaina's end: '<br>";
+#    die(mysqli_connect_error());
+#}
+
+
+if(!$connection){
+    echo "Could not connect to
+     MySQL on Gian's end: <br>";
+    die(mysqli_connect_error());
+}
 ?>
